51e8aca308b1de6bcc5c6a8f48facaf183652b1a
coco3167 <corent1.creully@sfr.fr>
2021-10-24 01:10:32 +0200

    Implémentation de base à debugger

--- JeuDeLaVieGraf/Form1.cs
+++ JeuDeLaVieGraf/Form1.cs
@@ -12,12 +12,31 @@ namespace JeuDeLaVieGraf
 {
     public partial class Form1 : Form
     {
+        private Game game;
+        private int nbAliveCell = 40*30;
+        public int n;
         public Form1()
         {
-            int n = 50;
+            n = 40;
             InitializeComponent(n);
+            game = new Game(n,nbAliveCell);
+            Timer MyTimer = new Timer();
+            MyTimer.Interval = (600);
+            MyTimer.Tick += new EventHandler(MyTimer_Tick);
+            MyTimer.Start();
         }
 
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            game.grid.UpdateGrid();
+            Refresh();
+        }
+        
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            game.Paint(e.Graphics);
+        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.Write("");
--- JeuDeLaVieGraf/Program.cs
+++ JeuDeLaVieGraf/Program.cs
@@ -1,16 +1,268 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace JeuDeLaVieGraf
 {
+    public struct Coords
+    {
+        public readonly int x;
+        public readonly int y;
+        public Coords(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+
+    public class Cell
+    {
+        public bool isAlive { get; private set; }
+
+        private bool nextState;
+
+        public Cell()
+        {
+            isAlive = false;
+        }
+        public Cell(bool state)
+        {
+            isAlive = state;
+        }
+
+        public void ComeAlive()
+        {
+            nextState = true;
+        }
+
+        public void PassAway()
+        {
+            nextState = false;
+        }
+
+        public void Update()
+        {
+            isAlive = nextState;
+        }
+    }
+    public class Grid
+    {
+        private int n;
+        public Cell[,] TabCells;
+
+        public Grid(int nbCells, List<Coords> AliveCellCords)
+        {
+            n = nbCells;
+            TabCells = new Cell[n, n];
+            for (int ord = 0; ord < n; ord++)
+            {
+                for (int abs = 0; abs < n; abs++)
+                {
+                    Coords coords = new Coords(abs, ord);
+                    bool isAlive = false;
+                    if (AliveCellCords.Contains(coords))
+                    {
+                        AliveCellCords.Remove(coords);
+                        isAlive = true;
+                    }
+
+                    TabCells[coords.y, coords.x] = new Cell(isAlive);
+                }
+            }
+        }
+
+        public int GetNbAliveNeighboor(Coords coords)
+        {
+            int x = coords.x;
+            int y = coords.y;
+            int sum = 0;
+
+            int[,] listNeighboor = new int[,]
+            {
+                {x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1},
+                {x - 1, y}, {x + 1, y},
+                {x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1}
+            };
+
+            for (int loop = 0; loop < 8; loop++)
+            {
+                int X = listNeighboor[loop,0];
+                int Y = listNeighboor[loop,1];
+                if(X < n && Y < n && X >= 0 && Y >= 0)
+                    sum += Convert.ToInt32(TabCells[Y, X].isAlive);
+            }
+
+        return sum;
+        }
+
+        public List<Coords> GetCoordsCellsAlive()
+        {
+            List<Coords> coordsList = new List<Coords>();
+            for (int ord = 0; ord < n; ord++)
+                for (int abs = 0; abs < n; abs++)
+                    if (TabCells[ord, abs].isAlive)
+                        coordsList.Add(new Coords(abs, ord));
+
+            return coordsList;
+        }
+
+        private string[] AffichageTexteCell(Cell cell)
+        {
+            string[] strings = new string[] {"+---","| "};
+            if (cell.isAlive)
+                strings[1] += "X ";
+            else
+                strings[1] += "  ";
+            return strings;
+        }
+        
+        public void DisplayGrid()
+        {
+            string[,] affichage = new string[n,3];
+            for (int ord = 0; ord < n; ord++)
+                for (int abs = 0; abs < n; abs++)
+                {
+                    string[] strings = AffichageTexteCell(TabCells[ord, abs]);
+                    for (int loop = 0; loop < 2; loop++)
+                        affichage[ord, loop] += strings[loop];
+                    if (abs == n - 1)
+                    {
+                        affichage[ord, 0] += "+";
+                        affichage[ord, 1] += "|";
+
+                    }
+                    if (ord == n - 1)
+                    {
+                        affichage[ord, 2] += "+---";
+                        if (abs == n - 1)
+                            affichage[ord, 2] += "+";
+                    }
+                }
+            
+
+            foreach (string ligne in affichage)
+            {
+                if(ligne != null)
+                    Console.WriteLine(ligne);
+            }
+        }
+
+        public void UpdateGrid()
+        {
+            for (int ord = 0; ord < n; ord++)
+            {
+                for (int abs = 0; abs < n; abs++)
+                {
+                    Coords coords = new Coords(abs, ord);
+                    int nbNeighboor = GetNbAliveNeighboor(coords);
+                    Cell cell = TabCells[coords.y, coords.x];
+                    if (!cell.isAlive)
+                    {
+                        if (nbNeighboor == 3)
+                        {
+                            cell.ComeAlive();
+                            //Console.Write("Apparition vie"); //Debug
+                        }
+                        else
+                        {
+                            cell.PassAway();
+                            //Console.Write("Rien de spécial"); //Debug
+                        }
+                    }
+                    else if (nbNeighboor != 2 && nbNeighboor != 3)
+                    {
+                        cell.PassAway();
+                        //Console.Write("Mort de la cellule"); //Debug
+                    }
+                    else
+                    {
+                        cell.ComeAlive();
+                        //Console.Write("La cellule survit"); //Debug
+                    }
+                    //Console.WriteLine($" nb Parents = {nbNeighboor} {coords}"); //Debug
+                }
+            }
+
+            foreach (Cell cell in TabCells)
+            {
+                cell.Update();
+            }
+        }
+    }
+    public class Game
+    {
+        private int n;
+        private int iter;
+        public Grid grid;
+        private readonly List<Coords> AliveCellsCoords = new List<Coords>();
+
+        public Game(int nbCells, int nbIterations, int nbAlive) : this(nbCells,nbAlive)
+        {
+            this.iter = nbIterations;
+        }
+        public Game(int nbCells, int nbAlive)
+        {
+            this.n = nbCells;
+            Random random = new Random();
+            for (int loop = 0; loop < nbAlive; loop++)
+            {
+                Coords coords = new Coords(random.Next(n), random.Next(n));
+                if (!AliveCellsCoords.Contains(coords))
+                    AliveCellsCoords.Add(coords);
+                else
+                    loop--;
+            }
+            grid = new Grid(n, AliveCellsCoords);
+        }
+
+        public void Paint(Graphics g)
+        {
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid.TabCells[i,j].isAlive)
+                    {
+                        g.FillRectangle(whiteBrush, j * 5, i * 5, 5, 5);
+                    }
+                }
+            }
+        }
+        
+        public void RunGameConsole()
+        {
+            for (int loop = 0; loop < iter; loop++)
+            {
+                if (grid.GetCoordsCellsAlive().Any())
+                {
+                    grid.DisplayGrid();
+                    grid.UpdateGrid();
+                    Console.ReadLine(); 
+                }
+            }
+            grid.DisplayGrid();
+        }
+    }
+    
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        
+        
+        
+        
+        
         [STAThread]
         static void Main()
         {
