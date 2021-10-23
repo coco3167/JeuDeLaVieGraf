29fef1d0f01623dd403c3278c91688d7032006ba
coco3167 <corent1.creully@sfr.fr>
2021-10-24 00:46:36 +0200

    Fenêtre stable

---
+++ JeuDeLaVieGraf/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeuDeLaVieGraf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int n = 50;
+            InitializeComponent(n);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.Write("");
+        }
+    }
+}
